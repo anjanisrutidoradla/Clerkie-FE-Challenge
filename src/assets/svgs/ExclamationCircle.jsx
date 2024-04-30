6566be0d9736372710f759690d22b99ce9561dfd
Anjani Sruti Doradla <anjanisruti.doradla@gmail.com>
2024-04-30 13:44:15 -0500

    Add ExclamationCircle asset

---
+++ src/assets/svgs/ExclamationCircle.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function ExclamationCircle() {
+  return (
+    <svg
+      className="w-[16px] h-[16px]"
+      width="24"
+      height="24"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fillRule="evenodd"
+        d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v5a1 1 0 1 0 2 0V8Zm-1 7a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H12Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export default ExclamationCircle;
